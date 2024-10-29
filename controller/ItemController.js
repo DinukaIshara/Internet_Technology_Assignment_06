bdf39e43567db4e79f6131bd663127b10e995a16
DinukaIshara <dinukaphoto692@gmail.com>
2024-10-29 20:26:01 +0530

    generate item code and set form it

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -1,2 +1,13 @@
 import ItemModel from "../models/itemModel.js";
 import {item_arr} from "../db/database.js";
+
+$(document).ready(function (){
+    $("#itemId").val(generatedId());
+});
+
+let generatedId = function generatedId(){
+    console.log(item_arr.length + 1);
+    let id = item_arr.length + 1;
+    return "I0" + id;
+
+}
