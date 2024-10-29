df5077bf49f49262e2c6322c2ba433a8d8f41bd5
DinukaIshara <dinukaphoto692@gmail.com>
2024-10-29 19:49:17 +0530

    generate customer id and set customerId

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -1,2 +1,13 @@
 import CustomerModel from "../models/customerModel.js";
 import {customer_arr} from "../db/database.js";
+
+$(document).ready(function (){
+    $("#customerId").val(generatedId());
+});
+
+let generatedId = function generatedId(){
+    console.log(customer_arr.length + 1);
+    let id = customer_arr.length + 1;
+    return "C0" + id;
+
+}
