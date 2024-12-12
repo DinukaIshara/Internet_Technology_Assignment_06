2abc1fd2360f3b5e6d1bee3f1b9209d8b6f5823d
DinukaIshara <dinukaphoto692@gmail.com>
2024-12-12 15:36:11 +0530

    add orderdetailscontroller

---
+++ controller/OrderDetailsController.js
@@ -0,0 +1,31 @@
+import {customer_arr, order_arr, order_detail_arr} from "../db/database.js";
+
+
+export function setOrdersTable(){
+    $("#orderTableBody").empty()
+    order_arr.map((order,number) => {
+        let data = `<tr>
+                           <td >${order._id}</td>
+                           <td >${order._cusId}</td>
+                           <td >${order._total}</td>
+                           <td >${order._date}</td>
+                           </tr>`
+        $("#orderTableBody").append(data);
+    })
+}
+
+export function setOrderDetailsTable(){
+    $("#orderDetailsTableBody").empty()
+
+
+    order_detail_arr.map((orderDetail,number) => {
+        let data = `<tr>
+                           <td >${orderDetail._orderId}</td>
+                           <td >${orderDetail._itemId}</td>
+                           <td >${orderDetail._qty}</td>
+                          
+                           
+                           </tr>`
+        $("#orderDetailsTableBody").append(data);
+    })
+}
